343aaab138349ef14ef3f0d3575bb4d417b3c05f
Lamine Dxallo <mamadou-lamine.diallo@laplateforme.io>
2025-05-22 10:19:27 +0200

    Initialize state for cards, choices, and interaction control

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import GameBoard from "./components/GameBoard";
+import "./App.css";
+
+const pokemonList = [
+  { name: "absol", img: "/assets/img/absol.png" },
+  { name: "armaldo", img: "/assets/img/armaldo.png" },
+  { name: "bulbizare", img: "/assets/img/bulbizare.png" },
+  { name: "totdile", img: "/assets/img/totdile.png" },
+  { name: "chenipan", img: "/assets/img/chenipan.png" },
+  { name: "electro", img: "/assets/img/electro.png" },
+  { name: "fennekin", img: "/assets/img/fennekin.png" },
+  { name: "gengar", img: "/assets/img/gengar.png" },
+  { name: "hericendre", img: "/assets/img/hericendre.png" },
+  { name: "pachirisu", img: "/assets/img/pachirisu.png" },
+  { name: "phampy", img: "/assets/img/phampy.png" },
+  { name: "vulpix", img: "/assets/img/vulpix.png" }
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const [cards, setCards] = useState([]);
+  const [firstChoice, setFirstChoice] = useState(null);
+  const [secondChoice, setSecondChoice] = useState(null);
+  const [disabled, setDisabled] = useState(false);
 }
-
-export default App
