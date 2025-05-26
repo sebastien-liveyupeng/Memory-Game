adefe3c4bd6575f5e21d6d16bfe5b56fc74468d2
Antoine <antoine.leca@laplateforme.io>
2025-05-26 11:31:44 +0200

    fix the react-helmet for the title of the pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
 
 import AppRouter from './Router.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppRouter />
+    <HelmetProvider>
+      <AppRouter />
+    </HelmetProvider>
   </StrictMode>,
 )
